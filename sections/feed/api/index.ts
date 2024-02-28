e1b96e2fb40ab63758abc8bdd32bd8b8aaef45fb
gill-dev <69636661+gill-dev@users.noreply.github.com>
2024-02-28 17:43:41 +0000

    Update index.ts

--- sections/feed/api/index.ts
+++ sections/feed/api/index.ts
@@ -11,14 +11,18 @@ type QueryParams = {
 export const feedApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getFeedItems: builder.query<FeedItem[], QueryParams>({
-      query: ({ accountUid, categoryUid, changesSince }) => ({
-        url: NEXTAPI.FEED.items,
-        params: {
-          accountUid,
-          categoryUid,
-          changesSince
-        },
-      }),
+      query: ({ accountUid, categoryUid, changesSince }) => {
+                const baseUrl = NEXTAPI.FEED.items;
+        const hasQueryParams = baseUrl.includes('?');
+                const queryParams = [
+          accountUid ? `accountUid=${accountUid}` : '',
+          categoryUid ? `categoryUid=${categoryUid}` : '',
+          changesSince ? `changesSince=${changesSince}` : ''
+        ].filter(Boolean).join('&'); 
+        const url = `${baseUrl}${hasQueryParams ? '&' : '?'}${queryParams}`;
+
+        return { url };
+      },
       transformResponse: (response: { feed: FeedItem[] }, meta, arg) => response.feed,
       providesTags: ['GetFeedItems'],
     }),
